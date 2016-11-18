a3ac063ed6e2fafa1ccaade66062d1a597214e47
Quickquant <Quickquant@gmail.com>
2016-11-18 10:13:35 -0600

    fixed issues with "final" commit

--- run_analysis.R
+++ run_analysis.R
@@ -2,20 +2,27 @@ getwd()
 library(data.table)
 library(dplyr)
 library(stringr)
+#Load libraries, read data in.
 fread("data/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
 fread("data/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-## merge data sets
-
 testDat <- fread("data/UCI HAR Dataset/test/subject_test.txt")
 trainDat <- fread("data/UCI HAR Dataset/train/subject_train.txt")
+## merge data sets
 mergeDat <- rbind(testDat, trainDat)
+## save the merged data sets in csv form 
 write.csv(mergeDat, "data/UCI HAR Dataset/merged/subject_merge.txt")
+
+#create a list to store file names, to reference in the loop
 inSigPrefixes <- list()
 inSigPrefixes$test  <- paste0("data/UCI HAR Dataset/test/Inertial Signals/",dir("data/UCI HAR Dataset/test/Inertial Signals"))
 inSigPrefixes$train <- paste0("data/UCI HAR Dataset/train/Inertial Signals/",dir("data/UCI HAR Dataset/train/Inertial Signals"))
 testSuffix <- "test.txt"
 trainSuffix <- "train.txt"
 masterTable <- NULL
+#main loop to merge data. Strategy: combine data sets, then take the mean and sd of them.
+#use that as one pair of measurements. Repeat this for all inertial signals.
+# we build masterTable with these pairs, then after the loop is complete, we write that to file.
+# as per the specifications, this output is to a txt, not a csv.
 for(i in 1:length(inSigPrefixes$test)){
   testData <- fread(inSigPrefixes$test[i])
   trainData <- fread(inSigPrefixes$train[i])
@@ -34,7 +41,7 @@ for(i in 1:length(inSigPrefixes$test)){
 }
 write.table(masterTable, file = "data/UCI HAR Dataset/merged/Inertial Signals/all_inertia_summary.txt")
 flatdata <- ydata <- xdata <- list()
-
+# we do similar combinations for the remaining non-inertial files, then write to another csv
 xdata$test <- fread("data/UCI HAR Dataset/test/X_test.txt")
 xdata$train <- fread("data/UCI HAR Dataset/train/X_train.txt")
 ydata$test <- fread("data/UCI HAR Dataset/test/Y_test.txt")
