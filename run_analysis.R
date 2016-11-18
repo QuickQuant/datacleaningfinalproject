a41a2606296600645b00072d211630438856cf55
Quickquant <Quickquant@gmail.com>
2016-11-18 08:48:47 -0600

    first commit!

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+getwd()
+fread("data/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
+fread("data/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
+## merge data sets
+
+testDat <- fread("data/UCI HAR Dataset/test/subject_test.txt")
+trainDat <- fread("data/UCI HAR Dataset/train/subject_train.txt")
+mergeDat <- rbind(testDat, trainDat)
+write.csv(mergeDat, "data/UCI HAR Dataset/merged/subject_merge.txt")
+inSigPrefixes <- list()
+inSigPrefixes$test  <- paste0("data/UCI HAR Dataset/test/Inertial Signals/",dir("data/UCI HAR Dataset/test/Inertial Signals"))
+inSigPrefixes$train <- paste0("data/UCI HAR Dataset/train/Inertial Signals/",dir("data/UCI HAR Dataset/train/Inertial Signals"))
+testSuffix <- "test.txt"
+trainSuffix <- "train.txt"
+masterTable <- NULL
+for(i in 1:length(inSigPrefixes$test)){
+  testData <- fread(inSigPrefixes$test[i])
+  trainData <- fread(inSigPrefixes$train[i])
+  mergeData <- rbind(testData, trainData)
+  flatData <- unlist(mergeData)
+  endMean <- mean(flatData)
+  endSD <- sd(flatData)
+  summaryMat <- matrix(c(endMean, endSD), 
+                       dimnames = list(c("mean","sd"), 
+                                       str_sub(dir("data/UCI HAR Dataset/test/Inertial Signals"),1,-10)[i])
+                )
+  masterTable <- cbind(masterTable, summaryMat)
+  write.table(summaryMat,
+              file = gsub(".txt", "_summary.csv", gsub("test", "merged", inSigPrefixes$test[i])))
+  write.csv(mergeData, file = gsub("txt", "csv", gsub("test", "merged", inSigPrefixes$test[i])))
+}
+flatdata <- ydata <- xdata <- list()
+
+xdata$test <- fread("data/UCI HAR Dataset/test/X_test.txt")
+xdata$train <- fread("data/UCI HAR Dataset/train/X_train.txt")
+ydata$test <- fread("data/UCI HAR Dataset/test/Y_test.txt")
+ydata$train <- fread("data/UCI HAR Dataset/train/Y_train.txt")
+flatdata$y <- unlist(ydata)
+flatdata$x <- unlist(xdata)
+endMean <- mean(unlist(flatData))
+  endSD <- sd(unlist(flatData))
+  summaryMat <- matrix(c(endMean, endSD), 
+                       dimnames = list(c("mean","sd"))
+                )
+write.csv(summaryMat,
+            file = "data/UCI HAR DatasetXYSummary.csv")
